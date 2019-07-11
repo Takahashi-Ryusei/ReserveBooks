af432376a5020c432a0833ff2e9d2285145f186c
160120 <160120@e0412-10.JC-21.local>
2019-07-11 10:16:27 +0900

    init commit.

---
+++ src/main/java/jp/te4a/spring/boot/reservebooks/ReserveBooksApplication.java
@@ -0,0 +1,13 @@
+package jp.te4a.spring.boot.reservebooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReserveBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReserveBooksApplication.class, args);
+	}
+
+}

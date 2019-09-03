cf5ccf4bb1f8600c80b899c652988a966adaa2d4
髙橋 流星 <160120@jc-21.jp>
2019-09-03 14:47:00 +0900

    merge

--- src/main/java/jp/te4a/spring/boot/reservebooks/ReserveBooksController.java
+++ src/main/java/jp/te4a/spring/boot/reservebooks/ReserveBooksController.java
@@ -1,18 +1,26 @@
 package jp.te4a.spring.boot.reservebooks;
-import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.validation.FieldError;
+
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("books")
 public class ReserveBooksController {
+	private static final Logger log = LoggerFactory
+			.getLogger(ReserveBooksController.class);
 //  @Autowired
 //  ReserveBooksService bookService;//
 //  @ModelAttribute 
@@ -35,9 +43,15 @@ public class ReserveBooksController {
 		return "books/reserve";
 	}
   
-  @RequestMapping(value="/check", method=RequestMethod.POST)
-  String list2(Model model, ReserveBooksForm form) {
+  @RequestMapping(value="/check",  method=RequestMethod.POST)
+  String list2(Model model, @Validated ReserveBooksForm form, BindingResult result ) {
 	  ReserveBooksForm bf=new ReserveBooksForm();
+	  if(result.hasErrors()) {
+		  for(FieldError err: result.getFieldErrors()){
+			  log.debug("error code = [" + err.getCode() + "]");
+			  }
+		  return "books/reserve";
+	  }
 	  bf.setTitle(form.getTitle());
 	  bf.setPublisher(form.getPublisher());
 	  bf.setWriter(form.getWriter());
@@ -45,8 +59,19 @@ public class ReserveBooksController {
 	  bf.setName(form.getName());
 	  bf.setTel(form.getTel());
 	  bf.setCall(form.getCall());
-	model.addAttribute("message","test");
+	  model.addAttribute("message","注文確認");
 	  model.addAttribute("book",bf);
 	  return "books/check";
+  }
+  
+  @RequestMapping(value="/list", method=RequestMethod.GET)
+  String list(Model model) {
+	  List<ReserveBooksForm> orderlist = new ArrayList<ReserveBooksForm>();
+	  orderlist.add(new ReserveBooksForm());
+	  orderlist.add(new ReserveBooksForm());
+	  orderlist.add(new ReserveBooksForm());
+	  
+	  model.addAttribute("orderlist",orderlist);
+	  return "books/list";
   }
 }
--- src/main/java/jp/te4a/spring/boot/reservebooks/ReserveBooksForm.java
+++ src/main/java/jp/te4a/spring/boot/reservebooks/ReserveBooksForm.java
@@ -1,15 +1,25 @@
 package jp.te4a.spring.boot.reservebooks;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 public class ReserveBooksForm {
+  @NotBlank(message ="タイトルは必ず入力してください")
+  @Size(min = 1, max = 20, message ="タイトルは１文字以上２０文字以内で入力してください") 
   private String title;
-  private String publisher;
+  
+  private String publisher;	
+  
   private String writer;
+  //@Size(min=1,message="冊数は必ず入力してください")
   private Integer number;
+  @NotBlank(message ="名前は必ず入力してください")
   private String name;
+  @NotBlank(message ="電話番号は必ず入力してください")
   private String tel;
   private Integer call;
 }

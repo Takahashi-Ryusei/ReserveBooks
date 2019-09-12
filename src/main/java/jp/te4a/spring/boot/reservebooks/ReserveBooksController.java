e88f58b3f26e633ec165167e32aa09eb9e280c3e
160819 <160819@e0412-03.JC-21.local>
2019-09-12 11:43:18 +0900

    topページ追加　暫定版

--- src/main/java/jp/te4a/spring/boot/reservebooks/ReserveBooksController.java
+++ src/main/java/jp/te4a/spring/boot/reservebooks/ReserveBooksController.java
@@ -1,4 +1,5 @@
 package jp.te4a.spring.boot.reservebooks;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
@@ -18,8 +19,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("books")
 public class ReserveBooksController {
-	private static final Logger log = LoggerFactory
-			.getLogger(ReserveBooksController.class);
+	private static final Logger log = LoggerFactory.getLogger(ReserveBooksController.class);
+
 //  @Autowired
 //  ReserveBooksService bookService;//
 //  @ModelAttribute 
@@ -33,58 +34,48 @@ public class ReserveBooksController {
 	 */
 	@ModelAttribute
 	ReserveBooksForm ReserveBooksForm() {
-			return new ReserveBooksForm();
+		return new ReserveBooksForm();
 	}
-		
+	
 	@GetMapping
-	String list(/*Model model*/) {
+	String top(/* Model model */) {
+		// model.addAttribute("books", bookService.findAll());
+		return "books/top";
+	}
+	
+	@RequestMapping(value = "/reserve", method = RequestMethod.GET)
+	String list() {
 		//model.addAttribute("books", bookService.findAll());
 		return "books/reserve";
 	}
-  
-  @RequestMapping(value="/check",  method=RequestMethod.POST)
-  String list2(Model model, @Validated ReserveBooksForm form, BindingResult result ) {
-	  ReserveBooksForm bf=new ReserveBooksForm();
-	  if(result.hasErrors()) {
-		  for(FieldError err: result.getFieldErrors()){
-			  log.debug("error code = [" + err.getCode() + "]");
-			  }
-		  return "books/reserve";
-	  }
-	  bf.setTitle(form.getTitle());
-	  bf.setPublisher(form.getPublisher());
-	  bf.setWriter(form.getWriter());
-	  bf.setNumber(form.getNumber());
-	  bf.setName(form.getName());
-	  bf.setTel(form.getTel());
-	  bf.setCall(form.getCall());
-	  model.addAttribute("message","注文確認");
-	  model.addAttribute("book",bf);
-	  return "books/check";
-  }
+	@RequestMapping(value = "/check", method = RequestMethod.POST)
+	String list2(Model model, @Validated ReserveBooksForm form, BindingResult result) {
+		ReserveBooksForm bf = new ReserveBooksForm();
+		if (result.hasErrors()) {
+			for (FieldError err : result.getFieldErrors()) {
+				log.debug("error code = [" + err.getCode() + "]");
+			}
+			return "books/reserve";
+		}
+		bf.setTitle(form.getTitle());
+		bf.setPublisher(form.getPublisher());
+		bf.setWriter(form.getWriter());
+		bf.setNumber(form.getNumber());
+		bf.setName(form.getName());
+		bf.setTel(form.getTel());
+		bf.setCall(form.getCall());
+		model.addAttribute("message", "注文確認");
+		model.addAttribute("book", bf);
+		return "books/check";
+	}
+	@RequestMapping(value = "/list", method = RequestMethod.POST)
+	String list(Model model) {
+		List<ReserveBooksForm> orderlist = new ArrayList<ReserveBooksForm>();
+		orderlist.add(new ReserveBooksForm());
+		orderlist.add(new ReserveBooksForm());
+		orderlist.add(new ReserveBooksForm());
 
-  @RequestMapping(value="/list", method=RequestMethod.GET)
-  String list(Model model) {
-	  List<ReserveBooksForm> orderlist = new ArrayList<ReserveBooksForm>();
-	  orderlist.add(new ReserveBooksForm());
-	  orderlist.add(new ReserveBooksForm());
-	  orderlist.add(new ReserveBooksForm());
-	  
-	  model.addAttribute("orderlist",orderlist);
-	  return "books/list";
-  }
-  @RequestMapping(value="/books", method=RequestMethod.POST)
-  String back(Model model, ReserveBooksForm form) {
-	  ReserveBooksForm bf=new ReserveBooksForm();
-	  bf.setTitle(form.getTitle());
-	  bf.setPublisher(form.getPublisher());
-	  bf.setWriter(form.getWriter());
-	  bf.setNumber(form.getNumber());
-	  bf.setName(form.getName());
-	  bf.setTel(form.getTel());
-	  bf.setCall(form.getCall());
-	model.addAttribute("message","test");
-	  model.addAttribute("book",bf);
-	  return "books/reserve";
-}
+		model.addAttribute("orderlist", orderlist);
+		return "books/list";
+	}
 }
